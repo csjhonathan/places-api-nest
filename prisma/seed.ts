5a9b1e9c9a3a28a97356edaefb9c395126178b39
csjhonathan <jhonathancarv.s@gmail.com>
2024-07-14 16:01:55 -0300

    feature: adiciona usuário de teste no seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,32 +1,47 @@
 import { PrismaClient } from "@prisma/client";
+import { EncryptionHelper } from "../src/helpers/libs/encryption";
 
 const prisma = new PrismaClient();
 
 async function dbSeed() {
   try {
     const places = await prisma.place.findMany();
+    const users = await prisma.user.findMany();
 
     if (places.length >= 2) {
-      throw "Places table have data!";
+      console.log("Places table have data!");
+    } else {
+      const places_names = [
+        "Museu do Amanhã",
+        "Quinta da Boa Vista",
+        "Praça XV",
+        "Praia de Copacabana",
+      ];
+
+      places_names.forEach(async (name) => {
+        await prisma.place.create({
+          data: {
+            city: "Rio de Janeiro",
+            state: "Rio de Janeiro",
+            name,
+          },
+        });
+        console.log(`Place:${name} added in database!`);
+      });
     }
 
-    const places_names = [
-      "Museu do Amanhã",
-      "Quinta da Boa Vista",
-      "Praça XV",
-      "Praia de Copacabana",
-    ];
-
-    places_names.forEach(async (name) => {
-      await prisma.place.create({
+    if (users.length >= 1) {
+      console.log("Users table have data!");
+    } else {
+      console.log("Creating test user...");
+      await prisma.user.create({
         data: {
-          city: "Rio de Janeiro",
-          state: "Rio de Janeiro",
-          name,
+          name: "Test",
+          email: "test@email.com",
+          password: EncryptionHelper.hashData("123456"),
         },
       });
-      console.log(`Place:${name} added in database!`);
-    });
+    }
 
     console.log("Seed completed");
   } catch (error) {
